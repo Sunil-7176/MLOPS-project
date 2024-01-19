fc861361e195d7a9a9286d91584f636f1f44c216
Sunil-7176 <sunil.m7176@gmail.com>
2024-01-20 01:25:26 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+#import logging
+from src.logger import logging
 #the fn how the error message should look like
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info() #All the details where the error is (lyk file name, line no etc)
@@ -19,4 +20,13 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message #print the error message
     
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomException(e,sys)
+    
+
+
 
